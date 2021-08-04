9de0fb54d039884b0ac8bc7d915fe4f47fad8b98
Valmira Nogueira <valmiranogueira@hotmail.com>
2021-08-04 12:36:59 -0300

    Add a namespace

--- api/run_api.sh
+++ api/run_api.sh
@@ -1,4 +1,6 @@
-kubectl delete pod api
-kubectl delete service api
+kubectl create namespace teste || true
+kubectl config set-context --current --namespace=teste
+kubectl delete pod api || true
+kubectl delete service api || true
 docker build -t api .
 kubectl create -f api.yaml
